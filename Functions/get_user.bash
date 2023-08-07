e98823d964ecbb47f7d45199648934ae9a086f3c
James Seiters <jseiters@atomic.computer>
2023-08-07 15:30:10 -0400

    Initial Github commit.

--- Functions/get_user.bash
+++ Functions/get_user.bash
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-############
-# Get User #
-############
+####################################################
+# Get User Framework Function                      #
+# Purpose: Get the user who is running the script. #
+# Inputs : None.                                   #
+# Outputs: Logged in user's name.                  #
+# Version: 0.0.1                                   #
+# Date   : 2023-27-07                              #
+####################################################
 
-# Pure BASH (Does not work for sudo.)
-#: \\u
-#printf '%s\n' "${_@P}"
-
-echo "${SUDO_USER:-$(logname)}"
+get_user() {
+    echo "${SUDO_USER:-$(logname)}"
+}
--- Functions/print_pretty.bash
+++ Functions/print_pretty.bash
@@ -1,26 +1,43 @@
 #!/bin/bash
 
-#####################
-# BASH Print Pretty #
-#####################
-
+#######################################################################
+# BASH Print Pretty                                                   #
+# Purpose: Prints colorized messages to the terminal.                 #
+# Inputs: $1 - The type of message to print. $2 - The message itself. #
+# Output : None.                                                       #
+# Version: 0.0.1                                                      #
+# Date: 2023-27-07                                                    #
+#######################################################################
 
 # Checks the first argument to determine what pattern to use. If the first argument is an error, warning, or default, then use that pattern.
 # If the first argument is an empty string, then use the default pattern. Otherwise, return an error..
 print_pretty() {
-    # Defaults
-    local red="\e[38;2;255;0;0m"
-    local yellow="\e[38;2;255;255;0m"
-    local nc="\e[0m"
-    local default_pattern='\n%s\n'
-    local error_pattern="\n${red}Error\x21${nc}\x20%s\n\n"
-    local warning_pattern="\n${yellow}Warning\x21${nc}\x20%s\n\n"
+	# Defaults
+	local red="\e[38;2;255;0;0m"
+	local yellow="\e[38;2;255;255;0m"
+	local nc="\e[0m"
+	local default_pattern='%s\n'
+	local error_pattern="${red}Error\x21${nc}\x20%s\n\n"
+	local warning_pattern="${yellow}Warning\x21${nc}\x20%s\n\n"
+	local wait_pattern='%s\u2026'
 
-    if [[ $1 == "error" ]]; then
-        printf "$error_pattern" "$2"
-    elif [[ $1 == "warning" ]]; then
-        printf "$warning_pattern" "$2"
-    else
-        printf "$default_pattern" "$2"
-    fi
+	case "$1" in
+		"error" )
+			local pattern="$error_pattern"
+			shift
+		;;
+		"warning" )
+			local pattern="$warning_pattern"
+			shift
+		;;
+		"wait" )
+			local pattern="$wait_pattern"
+			shift
+		;;
+		* )
+			local pattern="$default_pattern"
+		;;
+	esac
+
+	printf "$pattern" "$1"
 }
